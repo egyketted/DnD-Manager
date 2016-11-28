20a3fd1ad42d521fb4915f03afcc799d8d4c919f
Csaba Valyi <csaba.valyi1@gmail.com>
2016-11-28 21:52:56 +0100

    Export applciation module in app.js

--- public-src/app/app.js
+++ public-src/app/app.js
@@ -2,35 +2,6 @@
 require('angular');
 require('angular-ui-router');
 
-angular.module('DnDManagerApp', ['ui.router'])
-	.config(['$locationProvider', '$stateProvider', '$urlRouterProvider', function($locationProvider, $stateProvider, $urlRouterProvider) {
-		$locationProvider.html5Mode(true);
-		$stateProvider.state('home', {
-			url: '/',
-			views: {
-				'content': {
-					templateUrl: 'views/main.html',
-					controller: 'mainCtrl'
-				}
-			}
-		})
-		.state('login', {
-			url: '/login',
-			views: {
-				'content': {
-					templateUrl: 'views/main.html',
-					controller: 'mainCtrl'
-				}
-			}
-		})
-		.state('data', {
-			url: '/data',
-			views: {
-				content: {
-					templateUrl: 'views/data.html',
-					controller: 'dataCtrl'
-				}
-			}
-		});
-		$urlRouterProvider.otherwise('/');
-	}]);
+module.exports = {
+    appModule: angular.module('DnDManagerApp', ['ui.router'])
+};
