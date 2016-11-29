c12b82b6f5a0391aa8442a4372196e290dbc2041
Csaba Valyi <csaba_valyi@epam.com>
2016-11-29 09:56:57 +0100

    Indent main.routes.js

--- public-src/app/main/main.routes.js
+++ public-src/app/main/main.routes.js
@@ -4,14 +4,14 @@ var app = require('../app').appModule;
 var mainCtrl = require('./main.controller');
 
 app.config(['$stateProvider', function($stateProvider) {
-		$stateProvider.state('home', {
-                url: '/',
-                views: {
-                    'content': {
-                        templateUrl: 'app/main/main.html',
-                        controller: mainCtrl.ID,
-                        controllerAs: mainCtrl.ID
-                    }
-                }
-		    });
+    $stateProvider.state('home', {
+        url: '/',
+        views: {
+            'content': {
+                templateUrl: 'app/main/main.html',
+                controller: mainCtrl.ID,
+                controllerAs: mainCtrl.ID
+            }
+        }
+    });
 }]);
