bca754cce258f3d3f8a41cb2fa023b05e44310b0
Csaba Valyi <csaba_valyi@epam.com>
2016-11-29 09:22:02 +0100

    Disable html5 mode

--- public-src/app/app.js
+++ public-src/app/app.js
@@ -9,6 +9,5 @@ module.exports = {
 };
 
 app.config(['$locationProvider', '$urlRouterProvider', function($locationProvider, $urlRouterProvider) {
-    $locationProvider.html5Mode(true);
     $urlRouterProvider.otherwise('/');
 }]);
